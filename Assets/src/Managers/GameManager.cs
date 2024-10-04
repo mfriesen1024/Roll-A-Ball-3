2eb00a8eb7ca70d5fb32def8312bc2fee46e90ae
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-04 19:17:09 -0300

    Create GameManager.cs
    I need to add stuff to this but im lazy

---
+++ Assets/src/Managers/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollABall.Assets.src.Managers
+{
+    internal partial class GameManager:Node
+    {
+    }
+}
