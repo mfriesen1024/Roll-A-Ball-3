5894592e78310c72655e2af5df36c7943a697dd9
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-04 19:16:51 -0300

    Create PlayerManager.cs

---
+++ Assets/src/Player/PlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollABall.Assets.src.Player
+{
+    /// <summary>
+    /// Keeps track of and/or centralizes player stuff for encapsulation's sake.
+    /// </summary>
+    internal partial class PlayerManager:Node
+    {
+        #region Refs
+        /// <summary>
+        /// Singleton instance of the player.
+        /// </summary>
+        public static PlayerManager Instance {  get; private set; }
+
+        [Export]PlayerController controller;
+        #endregion
+    }
+}
