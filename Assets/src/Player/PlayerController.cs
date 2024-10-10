fe5b916d2214876faa437389ed21ddc9dd9e3b9c
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-09 21:29:13 -0300

    Create PlayerController.cs

---
+++ Assets/src/Player/PlayerController.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace RollABall.Assets.src.Player
+{
+    /// <summary>
+    /// Responsible for moving the player.
+    /// </summary>
+    internal partial class PlayerController : Node3D
+    {
+    }
+}
