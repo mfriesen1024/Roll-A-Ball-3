1bedf5128768893f5ddb154eb41a3a89d300003d
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-09 21:59:18 -0300

    Add right

--- Assets/src/Player/PlayerCam.cs
+++ Assets/src/Player/PlayerCam.cs
@@ -16,5 +16,11 @@ namespace RollABall.Assets.src.Player
             Vector2 forward2d = new Vector2(trueForward.X,trueForward.Z);
             return forward2d;
         }
+        public Vector2 Get2DRight()
+        {
+            Vector3 trueRight = Transform.Basis.X;
+            Vector2 right2d = new Vector2(trueRight.X,trueRight.Z);
+            return right2d;
+        }
     }
 }
