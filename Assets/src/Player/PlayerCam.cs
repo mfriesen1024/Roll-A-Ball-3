e0951b9c0a3cbf62b0b0581e465cfb5093e15681
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-10 18:06:43 -0300

    Update PlayerCam.cs
    This should now return the correct forward and right.

--- Assets/src/Player/PlayerCam.cs
+++ Assets/src/Player/PlayerCam.cs
@@ -12,13 +12,13 @@ namespace RollABall.Assets.src.Player
     {
         public Vector2 Get2DForward()
         {
-            Vector3 trueForward = Transform.Basis.Z;
+            Vector3 trueForward = GlobalBasis.Z;
             Vector2 forward2d = new Vector2(trueForward.X,trueForward.Z);
             return forward2d;
         }
         public Vector2 Get2DRight()
         {
-            Vector3 trueRight = Transform.Basis.X;
+            Vector3 trueRight = GlobalBasis.X;
             Vector2 right2d = new Vector2(trueRight.X,trueRight.Z);
             return right2d;
         }
