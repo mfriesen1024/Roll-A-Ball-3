b18df11610ddd7f2630fd44a99ad63a6ce27afba
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-10 17:28:37 -0300

    Add look rotation system.

--- Assets/src/Player/PlayerController.cs
+++ Assets/src/Player/PlayerController.cs
@@ -13,30 +13,47 @@ namespace RollABall.Assets.src.Player
 
         [Export] RigidBody3D ball;
         [Export] PlayerCam cam;
+        Node3D camParent;
         #endregion
 
         #region speeds
         [Export] float maxMoveSpeed = 10;
         [Export] float moveLerpMod = 1;
-        [Export] float mouseSensitivityMod = 0.01f;
+        [Export] float mouseSensitivityMod = 1f;
         #endregion
 
         #region moveFields
         Vector3 moveVector3D = Vector3.Zero;
+        Vector2 lookVector = Vector2.Zero;
         #endregion
 
         public override void _Ready()
         {
             playerLog = new Logger(true,true,"logs\\","playerLog","txt",true);
+
+            // Set refs.
+            camParent = (Node3D)cam.GetParent();
         }
 
         public override void _PhysicsProcess(double delta)
         {
-            Vector3 moveVelocityGoal = moveVector3D * maxMoveSpeed;
-            Vector3 newVelocity = ball.LinearVelocity.Lerp(moveVelocityGoal, (float)delta*moveLerpMod);
-            ball.LinearVelocity = newVelocity;
-
+            SetBallVelocity();
+            SetLookRotations();
             UpdateOurPosition();
+
+            void SetBallVelocity()
+            {
+                Vector3 moveVelocityGoal = moveVector3D * maxMoveSpeed;
+                Vector3 newVelocity = ball.LinearVelocity.Lerp(moveVelocityGoal, (float)delta * moveLerpMod);
+                ball.LinearVelocity = newVelocity;
+            }
+
+            void SetLookRotations()
+            {
+                RotateY(-(float)delta * lookVector.X * mouseSensitivityMod);
+                camParent.RotateX(-(float)delta * lookVector.Y * mouseSensitivityMod);
+                lookVector = Vector2.Zero; // Apparently we dont get an event when relative is zero.
+            }
         }
 
         /// <summary>
@@ -54,7 +71,7 @@ namespace RollABall.Assets.src.Player
 
         public void OnLook(Vector2 direction)
         {
-
+            lookVector = direction;
         }
 
         void UpdateOurPosition()
