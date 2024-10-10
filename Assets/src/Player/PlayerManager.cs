388d76c0db7b956f7825558997cfdf0a5945ae53
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-09 21:12:55 -0300

    Cleaning and reference changes.

--- Assets/src/Player/PlayerManager.cs
+++ Assets/src/Player/PlayerManager.cs
@@ -1,24 +1,20 @@
 ﻿using Godot;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RollABall.Assets.src.Player
 {
     /// <summary>
     /// Keeps track of and/or centralizes player stuff for encapsulation's sake.
     /// </summary>
-    internal partial class PlayerManager:Node
+    internal partial class PlayerManager : Node
     {
         #region Refs
         /// <summary>
         /// Singleton instance of the player.
         /// </summary>
-        public static PlayerManager Instance {  get; private set; }
+        public static PlayerManager Instance { get; private set; }
 
-        [Export]PlayerController controller;
+        [Export] PlayerController controller;
+        [Export] RigidBody3D ball;
         #endregion
     }
 }
