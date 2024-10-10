059dee02bebdad3aee3b150624285fbd71b191a7
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-09 21:26:49 -0300

    Create forward get method.

--- Assets/src/Player/PlayerCam.cs
+++ Assets/src/Player/PlayerCam.cs
@@ -7,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace RollABall.Assets.src.Player
 {
+    // This should be used to fetch the player's forward direction, and it's parent should be rotated with mouse movement.
     internal partial class PlayerCam:Camera3D
     {
+        public Vector2 Get2DForward()
+        {
+            Vector3 trueForward = Transform.Basis.Z;
+            Vector2 forward2d = new Vector2(trueForward.X,trueForward.Z);
+            return forward2d;
+        }
     }
 }
