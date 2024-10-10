d56154ce16e1731b67aea99c6193f1307f2d492b
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-10 13:37:33 -0300

    Create InputManager.cs

---
+++ Assets/src/Managers/InputManager.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollABall.Assets.src.Managers
+{
+    internal partial class InputManager:Node
+    {
+        public static InputManager instance { get; private set; }
+    }
+}
