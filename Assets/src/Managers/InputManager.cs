7901a76704ddb7eadf663e27ef4039ee730902fa
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-10 14:38:26 -0300

    Add movement input handler.

--- Assets/src/Managers/InputManager.cs
+++ Assets/src/Managers/InputManager.cs
@@ -15,6 +15,27 @@ namespace RollABall.Assets.src.Managers
         {
             if(Instance == null) { Instance = this; }
             else { QueueFree(); }
+        void KeyInput(InputEventKey iek)
+        {
+            // For now, we'll hardcode inputs. No idea if I'll change it around later or not.
+            switch (iek.PhysicalKeycode)
+            {
+                case Key.W:
+                case Key.A:
+                case Key.S:
+                case Key.D: HandleMoveInput(); break;
+                case Key.Escape:
+                default: break;
+            }
+        }
+
+        void HandleMoveInput()
+        {
+            Vector2 newMove = Vector2.Zero;
+            if (Gates.XOR(Input.IsKeyPressed(Key.S), Input.IsKeyPressed(Key.W))) { if (Input.IsKeyPressed(Key.S)) { newMove.X--; } else { newMove.X++; } }
+            if (Gates.XOR(Input.IsKeyPressed(Key.A), Input.IsKeyPressed(Key.D))) { if (Input.IsKeyPressed(Key.A)) { newMove.Y--; } else { newMove.Y++; } }
+
+            controller.OnMove(newMove);
         }
     }
 }
