274e479f27a9994d3c017833c287acbabff40ddc
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-09 21:59:02 -0300

    Figure out movement.

--- Assets/src/Player/PlayerController.cs
+++ Assets/src/Player/PlayerController.cs
@@ -7,5 +7,31 @@ namespace RollABall.Assets.src.Player
     /// </summary>
     internal partial class PlayerController : Node3D
     {
+        #region refs
+        [Export] RigidBody3D ball;
+        [Export] PlayerCam cam;
+        #endregion
+
+        public override void _Process(double delta)
+        {
+            
+        }
+
+        /// <summary>
+        /// Move the player.
+        /// </summary>
+        public void Move(Vector2 direction)
+        {
+            direction = direction.Normalized();
+
+            // Compose the move direction.
+            Vector2 moveVector = direction.Y*cam.Get2DForward();
+            moveVector += direction.X * cam.Get2DRight();
+        }
+
+        public void Look(Vector2 direction)
+        {
+
+        }
     }
 }
