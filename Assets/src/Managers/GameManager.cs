706642c3ac0a737640981007ca724161bbcb3cfc
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-13 19:04:20 -0300

    Connect UIMan to GM

--- Assets/src/Managers/GameManager.cs
+++ Assets/src/Managers/GameManager.cs
@@ -14,6 +14,9 @@ namespace RollABall.Assets.src.Managers
         /// Called after the ready callback, intended to allow managers to get references to each other after initialization finishes.
         /// </summary>
         public static Action postInit = delegate { };
+        /// <summary>
+        /// Singleton instance.
+        /// </summary>
         public static GameManager Instance { get; private set; }
 
         #region managerRefs
@@ -21,6 +24,8 @@ namespace RollABall.Assets.src.Managers
         [Export] PackedScene inputManPrefab;
         public static PlayerManager PlayerManager { get; private set; }
         [Export] PackedScene playerManPrefab;
+        public static UIManager UIManager { get; private set; }
+        [Export] PackedScene uiManPrefab;
         #endregion
 
         public override void _Ready()
@@ -38,12 +43,21 @@ namespace RollABall.Assets.src.Managers
             else { PlayerManager = playerManPrefab.Instantiate() as PlayerManager; AddChild(PlayerManager); }
             if (InputManager.Instance != null) { InputManager = InputManager.Instance; }
             else { InputManager = inputManPrefab.Instantiate() as InputManager; AddChild(InputManager); }
+            if (UIManager.Instance != null) { UIManager = uiManPrefab.Instantiate() as UIManager; AddChild(UIManager); }
         }
 
         private void SetGMSingleton()
         {
             if (Instance == null) { Instance = this; }
             else { QueueFree(); return; }
+        }
+
+        /// <summary>
+        /// We'll use this to start a shutdown sequence, allowing for persistent objects to be saved.
+        /// </summary>
+        internal void StartQuit()
+        {
+            throw new NotImplementedException();
         }
     }
 
