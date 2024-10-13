143abc5c49761e16d5bad1238594ee5cc83078cf
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-13 17:31:54 -0300

    Add ui elements and helpers.

--- Assets/src/Managers/UIManager.cs
+++ Assets/src/Managers/UIManager.cs
@@ -9,11 +9,19 @@ namespace RollABall.Assets.src.Managers
     /// </summary>
     internal partial class UIManager : Node
     {
+        #region refs
         GameManager gameManager;
         public static UIManager Instance { get; private set; }
         public UIState State { get => state; set { state = value; OnSetState(value); } }
         private UIState state;
         public Logger log;
+        #endregion
+        #region UIObjectRefs
+        Control mainMenu, options, levelSelect, loading;
+        //HUD hud;
+        Control levelComplete, levelFailure;
+        [Export] PackedScene mainMenuScene, optionsScene, levelSelectScene, loadingScene, hudScene, levelCompleteScene, levelFailureScene;
+        #endregion
 
         public override void _Ready()
         {
@@ -34,7 +42,7 @@ namespace RollABall.Assets.src.Managers
         {
             switch (value)
             {
-                case UIState.Main: 
+                case UIState.Main:
                 case UIState.Options:
                 case UIState.LevelSelect:
                 case UIState.Loading:
@@ -45,6 +53,45 @@ namespace RollABall.Assets.src.Managers
                     string s = $"State {value} is not implemented.";
                     log.Write(s); throw new NotImplementedException(s);
             }
+        }
+
+        void HideAll()
+        {
+            if (mainMenu != null) { mainMenu.QueueFree(); mainMenu = null; }
+            if (options != null) { options.QueueFree(); options = null; }
+            if (levelSelect != null) { levelSelect.QueueFree(); levelSelect = null; }
+            if (loading != null) { loading.QueueFree(); loading = null; }
+            //if (hud != null) { hud.QueueFree(); hud = null; }
+            if (levelComplete != null) { levelComplete.QueueFree(); levelComplete = null; }
+            if (levelFailure != null) { levelFailure.QueueFree(); levelFailure = null; }
+        }
+        void MainMenuHelper()
+        {
+            mainMenu = mainMenuScene.Instantiate() as Control;
+        }
+        void OptionsHelper()
+        {
+            options = optionsScene.Instantiate() as Control;
+        }
+        void LevelSelectHelper()
+        {
+            levelSelect = levelSelectScene.Instantiate() as Control;
+        }
+        void LoadingHelper()
+        {
+            loading = loadingScene.Instantiate() as Control;
+        }
+        void HudHelper()
+        {
+            //hud = hudScene.Instantiate() as Control;
+        }
+        void LevelCompleteHelper()
+        {
+            levelComplete = levelCompleteScene.Instantiate() as Control;
+        }
+        void LevelFailureHelper()
+        {
+            levelFailure = levelFailureScene.Instantiate() as Control;
         }
     }
 
