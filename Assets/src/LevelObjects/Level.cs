d1d89d87cdce08af4293af83679f07494d246385
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-15 17:42:36 -0300

    Start work on initializer for level

--- Assets/src/LevelObjects/Level.cs
+++ Assets/src/LevelObjects/Level.cs
@@ -1,4 +1,6 @@
 ﻿using Godot;
+using KeystoneUtils.Logging;
+using RollABall.Assets.src.LevelObjects.Obstacles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +14,29 @@ namespace RollABall.Assets.src.LevelObjects
     /// </summary>
     internal partial class Level:Node3D
     {
+        // Initialize with empty collection so we can .tolist later.
+        EndLevelTrigger[] triggers = [];
+        Checkpoint[] checkpoints = [];
+        MovingPlatform[] movingPlatforms = [];
 
+        public override void _Ready()
+        {
+            var elts = triggers.ToList();
+            var cps = checkpoints.ToList();
+            var mps = movingPlatforms.ToList();
+
+            foreach (var c in GetChildren())
+            {
+                Logger.StaticLogger.Write($"Node {c.Name} was detected. It has type {c.GetType()}.");
+                if (c is EndLevelTrigger) { elts.Add(c as EndLevelTrigger); }
+                else if (c is Checkpoint) { cps.Add(c as Checkpoint); }
+                else if (c is MovingPlatform) { mps.Add(c as MovingPlatform); }
+                else { Logger.StaticLogger.Write($"Node {c.Name} was not registered. It has type {c.GetType()}",LogLevel.info); }
+            }
+
+            triggers = elts.ToArray();
+            checkpoints = cps.ToArray();
+            movingPlatforms = mps.ToArray();
+        }
     }
 }
