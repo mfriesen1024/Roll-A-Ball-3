605dd69b8403a29affe79cf8b3efa05c93a7d574
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-15 17:42:18 -0300

    Update inheritance

--- Assets/src/LevelObjects/EndLevelTrigger.cs
+++ Assets/src/LevelObjects/EndLevelTrigger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Godot;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace RollABall.Assets.src.LevelObjects
     /// <summary>
     /// Ends the level when triggered.
     /// </summary>
-    internal class EndLevelTrigger
+    internal partial class EndLevelTrigger:Area3D
     {
     }
 }
