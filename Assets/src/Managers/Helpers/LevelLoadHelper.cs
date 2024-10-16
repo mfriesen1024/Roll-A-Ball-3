6c4cc4b3884b322e9e1be2a0b68572aa0f3262fc
mFriesen <144160470+mfriesen1024@users.noreply.github.com>
2024-10-16 17:45:10 -0300

    Add level loading stuff.

--- Assets/src/Managers/Helpers/LevelLoadHelper.cs
+++ Assets/src/Managers/Helpers/LevelLoadHelper.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Godot;
+using KeystoneUtils.Logging;
+using RollABall.Assets.src.LevelObjects;
+using System;
 using System.Threading.Tasks;
 
 namespace RollABall.Assets.src.Managers.Helpers
@@ -11,5 +11,38 @@ namespace RollABall.Assets.src.Managers.Helpers
     /// </summary>
     internal class LevelLoadHelper
     {
+        public static int activeLoadTasks { get; private set; } = 0;
+        public static int totalLoadTasks { get; private set; } = 0;
+        static Logger log = new Logger(true, true, "logs\\", "levelLoader", "txt", true);
+
+        public void LoadLevelAsync(PackedScene levelScene, Action<Level> callback)
+        {
+            Task.Run(() =>
+            {
+                // Loading screen crap.
+                IncrementTasks();
+
+                // Load the level.
+                Level level = levelScene.Instantiate() as Level;
+                callback(level);
+
+                // Loading screen end stuff.
+                DecrementTaskCount();
+            });
+        }
+
+        private void IncrementTasks()
+        {
+            activeLoadTasks++;
+            totalLoadTasks++;
+            log.WriteAll($"Starting a loadtask, this is task {activeLoadTasks}");
+        }
+
+        private void DecrementTaskCount()
+        {
+            activeLoadTasks--;
+            log.WriteAll($"Ended a loadtask, completing {totalLoadTasks-activeLoadTasks} of {totalLoadTasks}.");
+            if (activeLoadTasks == 0) { totalLoadTasks = 0; }
+        }
     }
 }
--- Assets/src/Managers/LevelManager.cs
+++ Assets/src/Managers/LevelManager.cs
@@ -1,16 +1,16 @@
 ﻿using Godot;
+using KeystoneUtils.Logging;
+using RollABall.Assets.src.LevelObjects;
+using RollABall.Assets.src.Managers.Helpers;
+using RollABall.Assets.src.Player;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RollABall.Assets.src.Managers
 {
     /// <summary>
     /// Manages levels, loading, and ui crap.
     /// </summary>
-    internal partial class LevelManager:Node
+    internal partial class LevelManager : Node
     {
         /// <summary>
         /// Singleton instance of the levelman.
@@ -20,7 +20,16 @@ namespace RollABall.Assets.src.Managers
         LevelLoadHelper loadHelper = new();
 
         [Export] PackedScene[] levels;
+        [Export] Texture2D[] levelTextures;
+
+        public Level activeLevel { get; private set; }
+
+        /// <summary>
+        /// Used to set the level to be loaded on a load request.
+        /// </summary>
         public int LevelIndex = 0;
+        public int CheckpointIndex = 0;
+
         public override void _Ready()
         {
             base._Ready();
@@ -30,5 +39,53 @@ namespace RollABall.Assets.src.Managers
             else { QueueFree(); return; }
         }
 
+        #region loading bunk
+        /// <summary>
+        /// Loads the level with index of LevelIndex, and spawns the player at the checkpoint with CheckpointIndex.
+        /// </summary>
+        public void Load()
+        {
+            // Set the load helper to load the level and set it's parent async.
+            loadHelper.LoadLevelAsync(levels[LevelIndex], (Level level) => { activeLevel = level; AddChild(level); });
+        }
+
+        private void Assign(Level level)
+        {
+            activeLevel = level;
+            level.TreeEntered += OnLoadFinish;
+            AddChild(level);
+        }
+
+        private void OnLoadFinish()
+        {
+            // Unsubscribe because performance.
+            activeLevel.TreeEntered -= OnLoadFinish;
+
+            // Set the player's position to checkpoint position.
+            Checkpoint checkpoint = activeLevel.checkpoints[CheckpointIndex];
+            PlayerManager.Instance.Ball.Position = checkpoint.Position + checkpoint.offset;
+            PlayerManager.Instance.Ball.LinearVelocity = Vector3.Zero;
+
+            // Set the ELTs to end the level.
+            foreach (EndLevelTrigger elt in activeLevel.triggers) { elt.BodyEntered += ELTCheck; }
+        }
+
+        /// <summary>
+        /// Discards the current level.
+        /// </summary>
+        public void Discard()
+        {
+            activeLevel.QueueFree();
+            activeLevel = null;
+        }
+        #endregion
+
+        /// <summary>
+        /// Checks if the object is the player ball, and if so, triggers a level win.
+        /// </summary>
+        private void ELTCheck(Node3D other)
+        {
+            }
+        }
     }
 }
