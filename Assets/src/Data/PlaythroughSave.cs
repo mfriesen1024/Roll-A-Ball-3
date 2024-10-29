79e943a806703addd7b108b93a6ada03dc06ce63
mFriesen <144160470+mfriesen1024@users.noreply.github.com>
2024-10-29 09:18:41 -0300

    Create files for serialization.

---
+++ Assets/src/Data/PlaythroughSave.cs
@@ -0,0 +1,41 @@
+﻿using KeystoneUtils.FileSystem.Binary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollABall.Assets.src.Data
+{
+    /// <summary>
+    /// Saves the player's progression for a given "run", but nothing that should persist between runs.
+    /// </summary>
+    internal class PlaythroughSave:IBinarySerializable<PlaythroughSave>
+    {
+        const byte reqBytes = 8;
+        public int level;
+        public int checkpoint;
+
+        public PlaythroughSave FromBinary(byte[] data)
+        {
+            PlaythroughSave save = new()
+            {
+                level = BitConverter.ToInt32(data),
+                checkpoint = BitConverter.ToInt32(data, 4)
+            };
+
+            return save;
+        }
+
+        public byte[] ToBinary(PlaythroughSave data)
+        {
+            byte[] bytes = [.. BitConverter.GetBytes(data.level), .. BitConverter.GetBytes(data.checkpoint)];
+            return bytes;
+        }
+
+        int IBinarySerializable<PlaythroughSave>.GetReqBytes()
+        {
+            return reqBytes;
+        }
+    }
+}
---
+++ Assets/src/Data/ProgressionSave.cs
@@ -0,0 +1,40 @@
+﻿using KeystoneUtils.FileSystem.Binary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollABall.Assets.src.Data
+{
+    /// <summary>
+    /// Saves anything that gets unlocked (levels for example)
+    /// </summary>
+    internal class ProgressionSave : IBinarySerializable<ProgressionSave>
+    {
+        const int reqBytes = 4;
+
+        int unlockedLevel = 0;
+
+        ProgressionSave IBinarySerializable<ProgressionSave>.FromBinary(byte[] data)
+        {
+            ProgressionSave save = new()
+            {
+                unlockedLevel = BitConverter.ToInt32(data)
+            };
+
+            return save;
+        }
+
+        int IBinarySerializable<ProgressionSave>.GetReqBytes()
+        {
+            return reqBytes;
+        }
+
+        byte[] IBinarySerializable<ProgressionSave>.ToBinary(ProgressionSave data)
+        {
+            byte[] bytes = [.. BitConverter.GetBytes(data.unlockedLevel)];
+            return bytes;
+        }
+    }
+}
