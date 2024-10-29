9f70e8553c17eb726ea76be59736579c3686ae69
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-29 14:45:57 -0300

    Use init/update/shutdown pattern.

--- Assets/src/Managers/DataManager.cs
+++ Assets/src/Managers/DataManager.cs
@@ -1,14 +1,7 @@
 ﻿using KeystoneUtils.FileSystem.Binary;
-using KeystoneUtils.Logging;
 using RollABall.Assets.src.Data;
 using System;
-using System.Collections.Generic;
-using System.Data;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace RollABall.Assets.src.Managers
 {
@@ -17,48 +10,62 @@ namespace RollABall.Assets.src.Managers
         string dataPath = "data\\";
         string savesPath = "saves\\";
         string scoresPath = "scores\\";
+        string runFName = "run.save";
+        string progressionFName = "progression.save";
+
+        // Data objects.
+        private PlaythroughSave runData = new();
+        private ProgressionSave progression = new();
 
         public void SaveScore(ScoreSave save)
         {
-            string fName = GetUniqueFileName(savesPath+scoresPath, "","score");
+            string fName = GetUniqueFileName(savesPath + scoresPath, "", "score");
 
             IBinarySerializable<ScoreSave> data = save;
 
             File.WriteAllBytesAsync(fName, data.ToBinary(save));
         }
 
-        public void SavePlaythrough(int level, int checkpoint, int lives)
+        public void RecordPlaythrough(int level, int checkpoint, int lives)
         {
-            // Define vars
-            string ptFName = "run.save";
-
-            IBinarySerializable<PlaythroughSave> blerg1 = new PlaythroughSave();
-
-            // Get previous saves.
-            PlaythroughSave playthrough = new();
-
-            playthrough.checkpoint=checkpoint;
-            playthrough.level=level;
-            playthrough.lives=lives;
-
-            File.WriteAllBytesAsync(savesPath+ptFName, blerg1.ToBinary(playthrough));
+            runData.checkpoint = checkpoint;
+            runData.level = level;
+            runData.lives = lives;
         }
 
         string GetUniqueFileName(string path, string name, object extension)
         {
             DateOnly dateOnly = DateOnly.FromDateTime(DateTime.Now);
             Directory.CreateDirectory(path);
-                int num = 1;
-                string finalName = $"{path}{name}-{dateOnly.ToShortDateString()}-{num}.{extension}";
-                while (File.Exists(finalName))
-                {
-                    num++;
-                    finalName = $"{path}{name}-{dateOnly.ToShortDateString()}-{num}.{extension}";
-                }
+            int num = 1;
+            string finalName = $"{path}{name}-{dateOnly.ToShortDateString()}-{num}.{extension}";
+            while (File.Exists(finalName))
+            {
+                num++;
+                finalName = $"{path}{name}-{dateOnly.ToShortDateString()}-{num}.{extension}";
+            }
 
-                File.Create(finalName).Close();
+            File.Create(finalName).Close();
 
             return finalName;
+        }
+
+        public void OnShutdown()
+        {
+            // Save everything before we close the game.
+
+            // Write stuff to files.
+            File.WriteAllBytesAsync(savesPath + runFName, runData.ToBinary(runData));
+            File.WriteAllBytes(savesPath + progressionFName, progression.ToBinary(progression));
+        }
+
+        public void OnStartup()
+        {
+            // Ensure saves exists, so we dont get DirectoryNotFounds.
+            Directory.CreateDirectory(savesPath);
+
+            if (File.Exists(savesPath+runFName)) { runData = runData.FromBinary(File.ReadAllBytes(savesPath + runFName)); }
+            if (File.Exists(savesPath+progressionFName)) { progression = progression.FromBinary(File.ReadAllBytes(savesPath + progressionFName)); }
         }
     }
 }
--- Assets/src/Managers/LevelManager.cs
+++ Assets/src/Managers/LevelManager.cs
@@ -92,7 +92,7 @@ namespace RollABall.Assets.src.Managers
                     if (PlayerManager.Instance.Ball.Equals(other))
                     {
                         CheckpointIndex = cp.index;
-                        GameManager.Instance.DataManager.SavePlaythrough(LevelIndex, CheckpointIndex, PlayerManager.Instance.Lives);
+                        GameManager.Instance.DataManager.RecordPlaythrough(LevelIndex, CheckpointIndex, PlayerManager.Instance.Lives);
                     }
                 };
             }
