5c64a3b2e9628de3c53051bd12a07206542d3c90
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-11-12 15:18:44 -0400

    Convert PlayerCam comments to XML

--- Assets/src/Player/PlayerCam.cs
+++ Assets/src/Player/PlayerCam.cs
@@ -1,4 +1,4 @@
-﻿using Godot;
+using Godot;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,15 +7,23 @@ using System.Threading.Tasks;
 
 namespace RollABall.Assets.src.Player
 {
-    // This should be used to fetch the player's forward direction, and it's parent should be rotated with mouse movement.
+    /// <summary>
+    /// This should be used to fetch the player's forward direction, and it's parent should be rotated with mouse movement.
+    /// </summary>
     internal partial class PlayerCam:Camera3D
     {
+        /// <summary>
+        /// Gets the forward direction and flattens it to a 2d vector, then normalizes it.
+        /// </summary>
         public Vector2 Get2DForward()
         {
             Vector3 trueForward = GlobalBasis.Z;
             Vector2 forward2d = new Vector2(trueForward.X,trueForward.Z);
             return forward2d.Normalized();
         }
+        /// <summary>
+        /// Gets the right direction and flattens it to a 2d vector, then normalizes it.
+        /// </summary>
         public Vector2 Get2DRight()
         {
             Vector3 trueRight = GlobalBasis.X;
